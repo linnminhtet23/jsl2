730ea1691ad5c01948d89e01ad945693e69ff94e
linnminhtet23@gmail.com <linnminhtet23@gmail.com>
2021-06-05 18:38:55 +0630

    master

---
+++ hiddensearch/js/app.js
@@ -0,0 +1,10 @@
+// Ui
+const search = document.querySelector('.search');
+const btn = document.querySelector('.btn');
+const input =document.querySelector('.input');
+
+// Event Listener
+btn.addEventListener('click',()=>{
+    // console.log("hay");
+    search.classList.toggle('active');
+})
