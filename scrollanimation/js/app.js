251f48cb651287f5128a407504ebc587e8cce7ba
linnminhtet23 <linnminhtet23@gmail.com>
2021-07-17 16:45:44 +0630

    master

--- scrollanimation/js/app.js
+++ scrollanimation/js/app.js
@@ -1,2 +1,9 @@
 //UI
-constboxes
+const boxes = document.querySelectorAll(".box");
+// console.log(boxes);
+function checkboxes(){
+    const innerHeight =window.innerHeight;
+    // console.log(innerHeight);
+
+}
+checkboxes
